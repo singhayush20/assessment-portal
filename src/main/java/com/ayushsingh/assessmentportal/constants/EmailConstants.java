4a951b4a66e974cc3360eecb1ad75c14b6230708
singhayush20 <ayushsingh20november@gmail.com>
2023-08-21 19:21:13 +0530

    New email credentials

--- src/main/java/com/ayushsingh/assessmentportal/constants/EmailConstants.java
+++ src/main/java/com/ayushsingh/assessmentportal/constants/EmailConstants.java
@@ -2,9 +2,8 @@ package com.ayushsingh.assessmentportal.constants;
 
 public class EmailConstants {
     //email service
-    public static final String MY_PASSWORD="lizypaovcdjznlzn";
-    public static final String FROM="ayush20april@gmail.com";
-    public static final String TO="as.ayush20apr@gmail.com";
+    public static final String MY_PASSWORD="lzuyvmyqwttottnp";
+    public static final String FROM="ayushsingh20april@gmail.com";
     //path of file to be sent in email -not required
     // public static final String FILE_PATH="C:\\Users\\Ayush Singh\\OneDrive\\Desktop\\New folder\\readme.txt";
 }
